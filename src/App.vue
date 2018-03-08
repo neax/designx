e58d4a70a789bf4a46c6a27f61e77dfe65423f95
Jorge Eduardo Macetas Huahuala <eduardo.macetas@gmail.com>
2018-03-08 17:01:43 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    
+    <Nav title="DesignX" icon="fas fa-cube">
+      <template slot="right">
+        <a href="#">Icon 1</a>
+      </template>
+    </Nav>
+
+    <main>
+      <h1 class="page-title">Cheasheet</h1>
+
+      <div class="grid">
+        <div class="grid-3">
+          <div class="sidebar">
+            <h3>Modules</h3>
+            <router-link :to="{ name: 'module', params: { moduleId: module.id }}" v-for="module in modules">
+              {{ module.title }}
+            </router-link>
+          </div>
+        </div>
+        <div class="grid-9">
+          <router-view></router-view>
+        </div>
+      </div>
+    </main>
+
+    <footer>
+      &copy; Neax
+    </footer>
+
+  </div>
+</template>
+
+<script>
+import Nav from './scripts/components/Nav'
+import Alert from './scripts/components/Alert'
+import Module from './scripts/custom/Module'
+
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      modules: require('./scripts/data/ModulesData').modules
+    }
+  },
+
+  components: {
+    Nav,
+    Module,
+    Alert
+  }
+}
+
+</script>
+
---
+++ src/scripts/components/Alert.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="alert" :class="colorClass">
+    <div class="alert-close" v-if="close"><i class="fas fa-times"></i></div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'alert',
+  computed: {
+    colorClass: function() {
+      const colorMap = {
+        success: 'green',
+        error: 'red',
+        warning: 'orange',
+        info: 'yellow',
+        debug: 'purple',
+      }
+
+      if(this.color in colorMap) {
+        return colorMap[this.color]
+      }
+      return this.color
+    }
+  },
+  props: {
+    color: {},
+    close: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
