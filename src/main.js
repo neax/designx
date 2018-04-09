5868400f2efe0eddc0823008ddfacd004ebbaf4b
Renato Guzman <rguznx@users.noreply.github.com>
2018-04-09 13:41:54 -0500

    Delete unused css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+require('normalize.css/normalize.css')
 require('./styles/designx.css')
 
 Vue.config.productionTip = false
